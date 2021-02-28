248d6c205ad0289dd08b1e203c402146747d09e2
Jay Jung <313jj2@gmail.com>
2021-02-28 11:53:20 -0500

    Section 5.6 – Structs

---
+++ ToDo List/ToDoItem.swift
@@ -0,0 +1,14 @@
+//
+//  ToDoItem.swift
+//  ToDo List
+//
+//  Created by Jay Jung on 2/27/21.
+//
+
+import Foundation
+
+struct ToDoItem {
+    var name: String
+    var date: Date
+    var notes: String
+}
